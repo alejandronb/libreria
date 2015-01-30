22421e8fb1d2d1daeabac3f2a74698cc9497b8a4
Alejandro <ale90nb@gmail.com>
2015-01-30 10:37:58 +0100

    Importar modelos

--- wsgi/openshift/libreria/views.py
+++ wsgi/openshift/libreria/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
 from django.shortcuts import render
-from .models import libro
+from libreria.models import libro
 
 
 def listar_libros(request):
 	libros = libro.objects.all()
 	return render(request, 'listar_libros.html', { 'libros': libros })
+    #return render(request, 'listar_libros.html', {})
